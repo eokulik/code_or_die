a25a98171b1af286788af76cbd670c6c598b630d
Aliaksei Murashka <amur@Aliakseis-MacBook-Pro.local>
2022-11-17 18:18:53 +0300

    added the first task solution

---
+++ amur/Homework_1/Task_1.py
@@ -0,0 +1,7 @@
+a = input('Please, set the first number. ')
+a = int(a)
+b = input('Please, set the second number. ')
+b = int(b)
+print('The sum of the values is ', a+b)
+print('The difference between the values is ', a-b)
+print('The product of the values is ', a*b)
