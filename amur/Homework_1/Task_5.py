cc2c0caf16961034c7ae94eba23acce15316c5e1
Aliaksei Murashka <amur@Aliakseis-MacBook-Pro.local>
2022-11-18 13:19:28 +0300

    fixed errors found by lint

--- amur/Homework_1/Task_5.py
+++ amur/Homework_1/Task_5.py
@@ -1,4 +1,4 @@
 x = input('Please, set the cube edge length, cm. ')
 x = abs(int(x))
-print('Side surface area of the cube is', 4*x**2,'cm2')
-print('Volume of the cube is', x**3,'cm3')
+print('Side surface area of the cube is', 4*x**2, 'cm2')
+print('Volume of the cube is', x**3, 'cm3')
