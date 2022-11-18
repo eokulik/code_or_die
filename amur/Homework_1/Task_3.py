af6a48b5b65d87ca78ef17d546df88b6246280ef
Aliaksei Murashka <amur@Aliakseis-MacBook-Pro.local>
2022-11-18 13:25:47 +0300

    fixed errors found by lint 2

--- amur/Homework_1/Task_3.py
+++ amur/Homework_1/Task_3.py
@@ -3,6 +3,6 @@ x = int(x)
 y = input('Please, set the second number. ')
 y = int(y)
 summ_x_y = x + y
-product_x_y = x*y
-print('Arithmetical mean is', summ_x_y/2)
-print('Geometric mean is', product_x_y**(1/2))
+product_x_y = x * y
+print('Arithmetical mean is', summ_x_y / 2)
+print('Geometric mean is', product_x_y ** (1 / 2))
