431867969a163c3e3145dc578de35b4ee5c73257
margaritasosenko <msashenka@kontur.io>
2022-11-21 18:12:48 +0300

    adding missing whitespaces

--- margarita/homework1/task3.py
+++ margarita/homework1/task3.py
@@ -4,10 +4,7 @@ b = 8
 numbers = []
 numbers.append(a)
 numbers.append(b)
-avg = sum(numbers)/len(numbers)
+avg = sum(numbers) / len(numbers)
 print('Среднее арифметическое: ', avg)
-avg_geom = (math.sqrt(a*b))
+avg_geom = (math.sqrt(a * b))
 print('Среднее геометрическое: ', avg_geom)
-
-
-
