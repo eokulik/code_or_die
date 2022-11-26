3bd6ec520dd6c7edb1bccc08cc7d95aed7d1e5cf
dmytriikazban <dmytrii.kazban@gmail.com>
2022-11-26 16:54:46 +0100

    Deleted the blank lines

--- Dmytrii Kazban/Homework_1/task_3.py
+++ Dmytrii Kazban/Homework_1/task_3.py
@@ -6,4 +6,3 @@ print('Here is the avg arithmetic of the "a and b"', ((a + b) / 2))
 x = ((a * b))
 y = x ** (0.5)
 print('Here is the avg geometric of the "a and b":', y)
-
--- Dmytrii Kazban/Homework_1/task_5.py
+++ Dmytrii Kazban/Homework_1/task_5.py
@@ -2,5 +2,3 @@ a = input('Provide the cube edge')
 
 print('The cube volume =', int(a) * int(a) * int(a))
 print('The side area of the cube =', (int(a) * int(a)) * 4)
-
-
