bb0733f91a0f843208a15c4afb016d284ba13060
Vadim Petrushkevich <vpetrushkevich@kontur.io>
2022-11-28 15:58:26 +0300

    Homework_1 done

---
+++ Vadim/homework_1/task3.py
@@ -0,0 +1,4 @@
+a = float(input('Enter first number: '))
+b = float(input('Enter second number: '))
+print('avg arithmetic =', (a + b) / 2)
+print('avg geometric =', (a * b) ** 0.5)
---
+++ Vadim/homework_1/task4.py
@@ -0,0 +1,4 @@
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+print('square =', (a * b) / 2)
+print('hypotenuse =', (a ** 2 + b ** 2) ** 0.5)
