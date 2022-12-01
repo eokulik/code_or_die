317c32f6600bf3bb8f9d13ced07e8d4a22dc66bf
Aliaksei Murashka <amur@Aliakseis-MacBook-Pro.local>
2022-12-01 15:40:42 +0100

    added Task_1 solution

---
+++ amur/Homework_3/Task_1.py
@@ -0,0 +1,13 @@
+text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+list_from_text = text.split()
+list_new_text = ''
+
+for word in list_from_text:
+    if word[-1].isalpha():
+        word += 'ing'
+    else:
+        word = word[:-1] + 'ing' + word[-1]
+    list_new_text = list_new_text + ' ' + word
+
+new_text = ''.join(list_new_text).lstrip()
+print(new_text)
