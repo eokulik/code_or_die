f53b5b33c52ab5b5247148ba4173d75b718e118c
Aliaksei Murashka <amur@Aliakseis-MacBook-Pro.local>
2022-12-02 12:24:55 +0100

    made error raising better in Task_2

--- amur/Homework_3/Task_2.py
+++ amur/Homework_3/Task_2.py
@@ -3,8 +3,7 @@ while True:
     user_number = input('Guess the number: ')
     for symbol in user_number:
         if symbol.isdigit() is False:
-            print('Please, enter an integer number!')
-            break
+            raise ValueError('Please, enter an integer number!')
     if number == int(user_number):
         print('Congratulations! You guessed it!')
         break
