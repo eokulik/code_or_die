cb9f6aff1342e3e242ccc8e02ced7f547eb2e18a
dmytriikazban <dmytrii.kazban@gmail.com>
2022-12-04 13:14:33 +0100

    Fixed issues

--- Dmytrii Kazban/Homework_2/task_1.py
+++ Dmytrii Kazban/Homework_2/task_1.py
@@ -1,16 +1,10 @@
-my_dict = {}
-my_dict['tuple'] = (1, 2, 3, 'str', False)
-my_dict['list'] = [1, 2, 3, 'test', True]
-my_dict['dict'] = {'one': 1, 'two': 2, 'Dmytrii': 'Kazban', 'four': 4, 'five': 5}
-my_dict['set'] = set()
-my_dict['set'].add(1)
-my_dict['set'].add(2)
-my_dict['set'].add(3)
-my_dict['set'].add(4)
-my_dict['set'].add(5)
-my_dict['str'] = 'Mauris fringilla odio sit amet pretium ultricies. Pellentesque habitant morbi tristique'
+my_dict = {'tuple': (1, 2, 3, 'str', False), 'list': [1, 2, 3, 'test', True],
+           'dict': {'one': 1, 'two': 2, 'Dmytrii': 'Kazban', 'four': 4, 'five': 5},
+           'set': {1, 2, 3, 4, 5},
+           'str': 'Mauris fringilla odio sit amet pretium ultricies. Pellentesque habitant morbi tristique'}
 
 
+print(my_dict)
 print(my_dict['tuple'][1:])
 
 
@@ -19,7 +13,7 @@ my_dict['list'].pop(4)
 print(my_dict['list'])
 
 
-my_dict['dict']['i am a tuple'] = 'i am not a tuple ;)'
+my_dict['dict']['i am a tuple'] = ('Java', 'JavaScript', 'Python')
 del my_dict['dict']['Dmytrii']
 print(my_dict['dict'])
 
@@ -30,7 +24,6 @@ print(my_dict['set'])
 
 
 print(my_dict['str'][:8])
-print(my_dict['str'].count('i'))
 print(my_dict['str'][8:12])
 print(my_dict['str'][::3])
 # my_dict['str'] = list(my_dict['str'])
@@ -38,6 +31,7 @@ print(type(my_dict['str']))
 a = my_dict['str'].split()
 print(a)
 print(a.index('pretium'))
+print(my_dict['str'].count('l'))
 
 
 print(my_dict)
