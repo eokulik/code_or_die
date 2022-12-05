45eb07a65f61bf331a1703d30eb950d3f7e8072a
dmytriikazban <dmytrii.kazban@gmail.com>
2022-12-05 10:53:39 +0100

    I hope i fixed the errors

--- Dmytrii Kazban/Homework_3/task_2.py
+++ Dmytrii Kazban/Homework_3/task_2.py
@@ -1,10 +1,9 @@
 number_to_guess = 8
 number_to_enter = int(input("Enter the number"))
 
-
 while number_to_enter != number_to_guess:
     if number_to_enter != number_to_guess:
         print("Try another number")
         number_to_enter = int(input("Enter the number"))
 else:
-        print("Congrats! You've guessed the number")
+    print("Congrats! You've guessed the number")
