cc76492d62c971b17f1cb8b18b89ed64a6e8e83e
dmytriikazban <dmytrii.kazban@gmail.com>
2022-12-05 10:44:02 +0100

    Что-то у меня не получается пока что 1ое задание.

---
+++ Dmytrii Kazban/Homework_3/task_1.py
@@ -0,0 +1,12 @@
+# Задание №1
+# Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову) в тексте “Etiam tincidunt neque erat, quis
+# molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero” и после этого
+# выводит получившийся текст на экран.
+
+str = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at,' \
+      ' dignissim vitae libero'
+
+
+for i in range(len(str)):
+      i = str.index()
+      print(i + 'ing')
