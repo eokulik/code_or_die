85f2a0377cbd48341b3fe660e8ca643527439bbc
Aliaksei Murashka <amur@Aliakseis-MacBook-Pro.local>
2022-12-05 14:49:43 +0100

    fixed names and extra actions at Task_1, if in Task_3

--- amur/Homework_3/Task_1.py
+++ amur/Homework_3/Task_1.py
@@ -2,14 +2,13 @@ text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. " \
        "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 
 list_from_text = text.split()
-list_new_text = ''
+new_text = ''
 
 for word in list_from_text:
     if word[-1].isalpha():
         word += 'ing'
     else:
         word = word[:-1] + 'ing' + word[-1]
-    list_new_text = list_new_text + ' ' + word
+    new_text = new_text + ' ' + word
 
-new_text = ''.join(list_new_text).lstrip()
-print(new_text)
+print(new_text.lstrip())
--- amur/Homework_3/Task_3.py
+++ amur/Homework_3/Task_3.py
@@ -1,17 +1,13 @@
-import math
-
 number = 0
 
 while number < 100:
     number += 1
-    num_3 = number / 3
-    num_5 = number / 5
 
-    if num_3 == math.ceil(num_3) and num_5 != math.ceil(num_5):
+    if number % 3 == 0 and number % 5 != 0:
         print('Fuzz')
-    elif num_3 != math.ceil(num_3) and num_5 == math.ceil(num_5):
+    elif number % 3 != 0 and number % 5 == 0:
         print('Buzz')
-    elif num_3 == math.ceil(num_3) and num_5 == math.ceil(num_5):
+    elif number % 3 == 0 and number % 5 == 0:
         print('FuzzBuzz')
     else:
         print(number)
