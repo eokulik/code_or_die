0efb4b94aec7d0699bcdee3d79dd291e974bdbcc
Anastasiya Zhdanovskaya <a.zhdanovskaya@objectstyle.com>
2022-12-09 19:08:48 +0300

    Anastasia: homework_4 done

---
+++ anastasia/homework_4/task_3.py
@@ -0,0 +1,14 @@
+# Выполните random.random() 15 раз, сохраняя куда-то каждый результат.
+# После этого выведите на экран максимальное из сгенеренных рандомов,
+# минимальное, сумму всех рандомов и их среднее арифметическое.
+
+from random import random
+
+random_numbers = []
+
+while len(random_numbers) < 15:
+    random_numbers.append(random())
+print('max -', max(random_numbers))
+print('min -', min(random_numbers))
+print('sum -', sum(random_numbers))
+print('avg -', sum(random_numbers)/len(random_numbers))
