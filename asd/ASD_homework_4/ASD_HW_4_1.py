b6c8cb0510e5b65ce9f72615dd577e87c48bb7bc
dudov48609 <dudov48609@yandex.ru>
2022-12-12 20:04:09 +0200

    HW_4 is done

---
+++ asd/ASD_homework_4/ASD_HW_4_1.py
@@ -0,0 +1,16 @@
+# Напишите программу. Есть две переменные, salary и bonus. Salary - int, bonus - bool.
+# Спросите у пользователя salary. А bonus пусть назначается рандомом.
+# Если bonus - true, то к salary должен быть добавлен рандомный бонус.
+# Примеры результатов:
+# 10000, True - '$10255'
+# 25000, False - '$25000'
+# 600, True - '$3785'
+import random
+import sys
+salary = int(input('Input salary $ '))
+bonus = bool(random.getrandbits(1))
+bonus_add = int(random.randint(0, sys.maxsize))
+if bonus is True:
+    print('$', (salary+bonus_add))
+else:
+    print('$', salary)
