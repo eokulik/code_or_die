df2c76db18a7b2592f12cd3e60b01b324b53bb20
eugeny.okulik <tiburon854@gmail.com>
2022-12-14 10:21:45 +0300

    Files form lesson_5: Functions

--- eugene/examples/first_app.py
+++ eugene/examples/first_app.py
@@ -1,1 +1,12 @@
 print('Hello World!')
+
+def append_to_dict(dictonary: dict, **kwargs):
+    '''
+    This function appends values given in kwargs to the given dictionary
+    '''
+    # print(kwargs)
+    for key, value in kwargs.items():
+        dictonary[key] = value
+    return dictonary
+
+print(append_to_dict(1, key1='c', key2='g'))
