5d48549b4cf65664fc9618bd0ca4673acade9bda
eugeny.okulik <tiburon854@gmail.com>
2022-12-14 10:23:51 +0300

    fixed lint

--- eugene/examples/first_app.py
+++ eugene/examples/first_app.py
@@ -1,5 +1,6 @@
 print('Hello World!')
 
+
 def append_to_dict(dictonary: dict, **kwargs):
     '''
     This function appends values given in kwargs to the given dictionary
@@ -8,5 +9,6 @@ def append_to_dict(dictonary: dict, **kwargs):
     for key, value in kwargs.items():
         dictonary[key] = value
     return dictonary
+
 
 print(append_to_dict(1, key1='c', key2='g'))
