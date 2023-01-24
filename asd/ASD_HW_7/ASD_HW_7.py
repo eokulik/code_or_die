97bdb96e97ee9ca7270cebfb30b1d13adbd879e7
dudov48609 <dudov48609@yandex.ru>
2023-01-24 18:48:58 +0200

    HW_5_1 (some changes after review)

---
+++ asd/ASD_HW_7/ASD_HW_7.py
@@ -0,0 +1,3 @@
+def calc(first, second, operation):
+    if opertaion == '+':
+    .....
--- asd/ASD_homework_5/ASD_HW_5_1.py
+++ asd/ASD_homework_5/ASD_HW_5_1.py
@@ -1,39 +1,44 @@
-
-
-def calc():
-    operation = int(input('''
-Пожалуйста, выберите желаемую операцию:
-1.Сложение
-2.Вычитание
-3.Умножение
-4.Деление
-и введите номер пункта меню'''))
-    numb1 = float(input('Введите первое число: '))
-    numb2 = float(input('Введите второе число: '))
-
-    if operation not in range(1, 5):
-        print("Неправильный оператор, попробуйте снова")
-        again()
-
-    else:
-
+def calc(numb1, numb2, operation):
+    if operation == 1:
         print('''
-Ваш результат: ''')
-        if operation == 1:
-            print('{} + {} = '.format(numb1, numb2))
-            print(numb1 + numb2)
-        elif operation == 2:
-            print('{} - {} = '.format(numb1, numb2))
-            print(numb1 - numb2)
-        elif operation == 3:
-            print('{} * {} = '.format(numb1, numb2))
-            print(numb1 * numb2)
-        elif operation == 4:
-            if numb2 != 0:
-                print("Частное: ", int(numb1 // numb2))
-                print("Остаток: ", int(numb1 % numb2))
-            else:
-                print("Деление на ноль!")
+        Ваш результат: ''')
+        print('{0} + {1} = '.format(numb1, numb2))
+        print(numb1 + numb2)
+    elif operation == 2:
+        print('''
+        Ваш результат: ''')
+        print('{0} - {1} = '.format(numb1, numb2))
+        print(numb1 - numb2)
+    elif operation == 3:
+        print('''
+        Ваш результат: ''')
+        print('{0} * {1} = '.format(numb1, numb2))
+        print(numb1 * numb2)
+    elif operation == 4:
+        if numb2 != 0:
+            print('''
+            Ваш результат: ''')
+            print("Частное: ", int(numb1 // numb2))
+            print("Остаток: ", int(numb1 % numb2))
+        else:
+            print("Деление на ноль!")
+
+
+def operation():
+    operation = int(input('''
+    Пожалуйста, выберите желаемую операцию:
+    1.Сложение
+    2.Вычитание
+    3.Умножение
+    4.Деление
+    и введите номер пункта меню
+    '''))
+    if operation not in range(1, 5):
+        print("Неправильный оператор")
+    else:
+        numb1 = float(input('Введите первое число: '))
+        numb2 = float(input('Введите второе число: '))
+        calc(numb1, numb2, operation)
 
 
 def again():
@@ -41,15 +46,16 @@ def again():
 Хотите попробовать снова?
 Если да, введите  +  и если нет то  - .
 ''')
-
     if calc_again.upper() == '+':
-        calc()
+        operation()
     elif calc_again.upper() == '-':
         print('До встречи')
-    else:
-        again()
+        exit()
 
 
-calc()
-while 1 == 1:
+operation()
+
+while True:
     again()
+
+
