6b24135bd15571e4c3a24e1e00285c1f79a94bec
dudov48609 <dudov48609@yandex.ru>
2023-03-01 14:46:24 +0200

    HW_7 (changes after review))

--- asd/ASD_homework_7/ASD_HW_7.py
+++ asd/ASD_homework_7/ASD_HW_7.py
@@ -1,30 +1,47 @@
-
-first = float(input('Введите первое число: '))
-
-second = float(input('Введите второе число: '))
-
-operation = []
+first_number = float(input('Введите первое число: '))
+second_number = float(input('Введите второе число: '))
 
 
 def my_dec(func):
-    def wrapper(first, second):
-        if first == second:
-            return first + second
-        elif first < 0 or second < 0:
-            return first * second
-        elif first > second:
-            return second - first
-        elif first < second:
-            return first / second
-        func(first, second, operation)
-    print(wrapper(first, second))
+    def wrapper(numb1, numb2):
+        if numb1 < 0 or numb2 < 0:
+            operation = 3
+        elif numb1 == numb2:
+            operation = 1
+        elif numb1 > numb2:
+            operation = 2
+        elif numb1 < numb2:
+            operation = 4
+        func(numb1, numb2, operation)
+    # print(wrapper(first_number, second_number, operation))
     return wrapper
 
 
 @my_dec
-def calc(first, second, operation):
+def calc(numb1, numb2, operation):
     if operation == 1:
-        return first + second
+        print('''
+        Ваш результат: ''')
+        print('{0} + {1} = '.format(numb1, numb2))
+        print(numb1 + numb2)
+    elif operation == 2:
+        print('''
+        Ваш результат: ''')
+        print('{1} - {0} = '.format(numb1, numb2))
+        print(numb1 - numb2)
+    elif operation == 3:
+        print('''
+        Ваш результат: ''')
+        print('{0} * {1} = '.format(numb1, numb2))
+        print(numb1 * numb2)
+    elif operation == 4:
+        if numb2 != 0:
+            print('''
+            Ваш результат: ''')
+            print("Частное: ", int(numb1 // numb2))
+            print("Остаток: ", int(numb1 % numb2))
+        else:
+            print("Деление на ноль!")
 
 
-calc(first, second)
+calc(first_number, second_number)
